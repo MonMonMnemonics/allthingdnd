54675b4252c7354a7c1d70cf86a42515fe0df4f7
Cendikia Abdi <cendekiaabdi@gmail.com>
2026-02-21 13:10:43 +0900

    retest autodeploy

--- apps/dndate/src/app/pages/Homepage.tsx
+++ apps/dndate/src/app/pages/Homepage.tsx
@@ -34,7 +34,7 @@ export function HomePage() {
                 <div className="w-px mx-2 bg-white border-0"/>
                 <div className="flex flex-col gap-2">
                     <div className="text-center text-7xl align-middle grow mb-3 font-bold">DNDate</div>
-                    <h1 className="w-full text-center">For when you want to dnd but just can't agree on a date, now with workspace!</h1>
+                    <h1 className="w-full text-center">For when you want to dnd but just can't agree on a date, now with workspace2!</h1>
                     <h1 className="w-full text-center">Inspired by <a href="https://tonton.amaneku.com/" target="_blank" className="font-medium text-fg-brand underline hover:no-underline">Tonton Amaneku</a></h1>
                     <div className="flex flex-row justify-center">
                         <button className="dark-button p-3 text-2xl rounded border flex items-center justify-center gap-2 font-light flex flex-row gap-2 items-center"
