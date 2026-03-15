17aa6d6c81f030e144fe667294a9feaf9be5a97b
Cendikia Abdi <cendekiaabdi@gmail.com>
2026-03-15 21:43:11 +0900

    test cicd2

--- apps/puzzles/src/routes/+page.svelte
+++ apps/puzzles/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <div class="flex flex-col h-screen w-screen overflow-hidden">
     <div class="flex flex-row w-full my-auto">
         <div class="mx-auto font-jacquard rounded px-7 py-5 border border-3 rounded-2xl flex flex-col gap-3 select-none">
-            <div class="text-8xl w-full text-center">Puzzles TEST CICD</div>
+            <div class="text-8xl w-full text-center">Puzzles TEST CICD 2</div>
             <hr/>
             <ul class="list-disc ms-7">
                 <li class="text-4xl cursor-pointer hover:text-blue-400 transition duration-500"><a href="/rune-wheel">Interconnected Rune Wheels</a></li>
